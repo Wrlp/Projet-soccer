573a24c6c1339ee655f547ac068784e36b1fc3a9
Wrlp <laure.warlop@gmail.com>
2026-05-17 20:13:16 -0400

    update download_data

--- download_data.py
+++ download_data.py
@@ -2,19 +2,21 @@ from SoccerNet.Downloader import SoccerNetDownloader
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 mySoccerNetDownloader = SoccerNetDownloader(LocalDirectory="data/soccernet")
 
 # Les annotations
-mySoccerNetDownloader.downloadGames(
-    files=["Labels-v2.json"], 
-    split=["train", "valid", "test"]
-)
+# mySoccerNetDownloader.downloadGames(
+#     files=["Labels-v2.json"], 
+#     split=["train", "valid", "test"]
+# )
 
 # Les features préextraites PCA512
-mySoccerNetDownloader.downloadGames(
-    files=["1_ResNET_TF2_PCA512.npy", "2_ResNET_TF2_PCA512.npy"], 
-    split=["train", "valid", "test"]
-)
+# mySoccerNetDownloader.downloadGames(
+#     files=["1_ResNET_TF2_PCA512.npy", "2_ResNET_TF2_PCA512.npy"], 
+#     split=["train", "valid", "test"]
+# )
 
 # Vidéos 224p 
 password = os.getenv("SOCCERNET_PASSWORD")
